e1e524be40abf093b527a21366475095cf237333
Mateus Veloso <mfvelosos@gmail.com>
2019-07-06 17:44:30 -0300

    Update links controller to find link by short param and redirect to source link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show]
 
   # GET /links
   # GET /links.json
@@ -10,15 +10,15 @@ class LinksController < ApplicationController
   # GET /links/1
   # GET /links/1.json
   def show
-  end
-
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit
+    if params[:short]
+      @link = Link.find_by(short: params[:short])
+      if redirect_to @link.source_url
+        @link.clicks += 1
+        @link.save
+      end
+    else
+      @link = Link.find(params[:id])
+    end
   end
 
   # POST /links
@@ -29,6 +29,7 @@ class LinksController < ApplicationController
     respond_to do |format|
       if @link.save
         format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new }
@@ -37,38 +38,12 @@ class LinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
-  def destroy
-    @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_link
-      @link = Link.find(params[:id])
+      @link = Link.find_by(short: params[:short])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:source_url, :short, :clicks, :title)
+      params.require(:link).permit(:source_url)
     end
 end
