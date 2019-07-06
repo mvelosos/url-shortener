603b625ab146cc550a3dbdd628832abc1a64bb13
Mateus Veloso <mfvelosos@gmail.com>
2019-07-06 17:04:32 -0300

    Add gem annotate to create models annotations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :bigint           not null, primary key
+#  source_url :string
+#  short      :string
+#  clicks     :integer          default(0)
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Link < ApplicationRecord
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id         :bigint           not null, primary key
+#  source_url :string
+#  short      :string
+#  clicks     :integer          default(0)
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
