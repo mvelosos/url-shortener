0f975452e19168ab3e9b7acfe8eb12958e6ea4b9
Mateus Veloso <mfvelosos@gmail.com>
2019-07-06 17:52:57 -0300

    Create methods to list all links and top clickeds links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+
+  def index
+    @link = Link.new
+    @top_links = Link.order(clicks: :desc).first(10)
+  end
+
+  def all
+    @link = Link.new
+    @links = Link.paginate(:page => params[:page], :per_page => 5)
+  end
+
 end
