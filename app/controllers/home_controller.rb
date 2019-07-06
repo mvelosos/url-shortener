8b10933488eeb0f72c3552a9be996e88a728c869
Mateus Veloso <mfvelosos@gmail.com>
2019-07-06 19:13:17 -0300

    Fetch 100 most populates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @link = Link.new
-    @top_links = Link.order(clicks: :desc).first(10)
+    @top_links = Link.order(clicks: :desc).first(100)
   end
 
   def all
     @link = Link.new
-    @links = Link.paginate(:page => params[:page], :per_page => 5)
+    @links = Link.paginate(:page => params[:page], :per_page => 10)
   end
 
 end
