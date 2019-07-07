39a2da5b8816268095575449ce8c4d469dfc6492
Mateus Veloso <mfvelosos@gmail.com>
2019-07-06 21:13:14 -0300

    Perform link model tests

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -14,5 +14,23 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with source_url attribute' do
+    link = build(:link)
+    expect(link).to be_valid
+  end
+
+  it 'is not valid without source_url attribute' do
+    link = build(:link, source_url: nil)
+    expect(link).to_not be_valid
+  end
+
+  it 'has short url when is created' do
+    link = create(:link)
+    expect(link.short).to be_truthy
+  end
+
+  it 'has clicks count equal 0' do
+    link = create(:link)
+    expect(link.clicks).to eql(0)
+  end
 end
